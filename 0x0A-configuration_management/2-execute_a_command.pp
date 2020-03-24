bd910f3012eb2b92f874b4eadcba6a2966fb6223
Luz <zulsb2093@gmail.com>
2020-03-24 15:25:23 +0000

    Correction task again.

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Kill process named killmenow.
 exec { 'killmenow':
-      command => 'pkill killmenow',
-      path    => '/usr/bin/'
+  command => 'pkill -f ./killmenow',
+  path    => '/usr/bin/',
 }
