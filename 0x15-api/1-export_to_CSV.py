b008511a7fc6d18291aaf8f2d9cf22a02598a46f
Luz <zulsb2093@gmail.com>
2020-05-04 22:35:55 +0000

    Add script that export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module that gather data from an API"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    users = requests.get("https://jsonplaceholder.typicode.com/users",
+                         params={"id": int(argv[1])})
+    username = users.json()[0].get("username")
+
+    to_dos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                          params={"userId": int(argv[1])})
+    list_dict = to_dos.json()
+    total_all_task = len(list_dict)
+
+    for d in list_dict:
+        d["username"] = username
+        d.pop("id")
+
+    with open("{}.csv".format(argv[1]), "w", newline="") as csvfile:
+        fieldnames = ["userId", "username", "completed", "title"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+
+        for d in list_dict:
+            writer.writerow(d)
