3398e0c2b461e8cfc507c975a57a1916074bcd33
Luz <zulsb2093@gmail.com>
2020-05-08 02:12:11 +0000

    Add recursive function returns a list containing the titles of all hot articles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,9 +8,9 @@ def recurse(subreddit, hot_list=[], after=None):
     """Return all hot articles for a given subreddit and
        return None if invalid subreddit given"""
 
-    user_agent = {"User-agent": "Luz"}
+    headers = {"User-agent": "Luz"}
     r = requests.get("http://www.reddit.com/r/{}/hot.json?after={}".
-                     format(subreddit, after), headers=user_agent)
+                     format(subreddit, after), headers=headers)
 
     if r:
         r = r.json().get("data")
